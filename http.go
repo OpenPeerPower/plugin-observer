2586406ab98328aedcf8dc1f36498152cfba0d65
Paul Caston <paul@caston.id.au>
2021-02-13 08:49:54 +0800

    initial takeup

---
+++ http.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"log"
+	"net"
+	"net/http"
+	"regexp"
+	"strings"
+)
+
+func checkNetwork(r *http.Request) bool {
+	remote := strings.Split(r.RemoteAddr, ":")[0]
+	remoteIP := net.ParseIP(remote)
+
+	// Is in network
+	if !oppioNetwork.Contains(remoteIP) {
+		log.Printf("Access not allow from %s", remote)
+		return false
+	}
+
+	return true
+}
+
+func apiPing(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
+
+func apiLogs(w http.ResponseWriter, r *http.Request) {
+	if !checkNetwork(r) {
+		http.Error(w, http.StatusText(http.StatusForbidden), http.StatusForbidden)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+		return
+	}
+	log.Printf("Access to logs from %s", r.RemoteAddr)
+
+	err := supervisorLogs(w)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Return the content
+	w.Header().Add("Content-Type", "text/plain")
+}
+
+func apiRestart(w http.ResponseWriter, r *http.Request) {
+	if !checkNetwork(r) || supervisorPing() {
+		http.Error(w, http.StatusText(http.StatusForbidden), http.StatusForbidden)
+		return
+	}
+	if r.Method != "POST" {
+		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+		return
+	}
+	log.Printf("Access to restart from %s", r.RemoteAddr)
+
+	err := supervisorRestart()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Return the content
+	w.WriteHeader(http.StatusOK)
+}
+
+type statusData struct {
+	On   bool
+	Logs string
+}
+
+func statusIndex(w http.ResponseWriter, r *http.Request) {
+	data := statusData{
+		On:   supervisorPing(),
+		Logs: "",
+	}
+
+	// Set logs
+	if !data.On {
+		var buf bytes.Buffer
+		var re = regexp.MustCompile(`\[\d+m`)
+		logWriter := bufio.NewWriter(&buf)
+
+		err := supervisorLogs(logWriter)
+		if err != nil {
+			data.Logs = err.Error()
+		} else {
+			data.Logs = re.ReplaceAllLiteralString(buf.String(), "")
+		}
+	}
+
+	// Render Website
+	indexTemplate.Execute(w, data)
+}
---
+++ supervisor.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"context"
+	"io"
+	"log"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/pkg/stdcopy"
+)
+
+func supervisorPing() bool {
+	response, err := httpClient.Get("http://supervisor/supervisor/ping")
+	if err != nil {
+		log.Printf("Supervisor ping failed with error %s", err)
+		return false
+	}
+
+	// Check response
+	if response.StatusCode < 300 {
+		return true
+	}
+
+	log.Printf("Supervisor ping failed with %d", response.StatusCode)
+	return false
+}
+
+func supervisorLogs(w io.Writer) error {
+	// Read logs from container
+	reader, err := cli.ContainerLogs(context.Background(), "oppio_supervisor", types.ContainerLogsOptions{
+		ShowStdout: true,
+		ShowStderr: true,
+		Follow:     false,
+		Timestamps: false,
+		Tail:       "all",
+	})
+
+	if err != nil {
+		log.Printf("Can't get supervisor logs %s", err)
+		return err
+	}
+	defer reader.Close()
+
+	// Return the content
+	_, err = stdcopy.StdCopy(w, w, reader)
+	return err
+}
+
+func supervisorRestart() error {
+	// Read logs from container
+	err := cli.ContainerStop(context.Background(), "oppio_supervisor", nil)
+	if err != nil {
+		log.Printf("Can't stop supervisor: %s", err)
+		return err
+	}
+
+	return nil
+}
